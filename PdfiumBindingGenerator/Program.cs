1093d2f5cf4b2f55a0bc8959ed012f1b70c72ead
modest-as <modestas.petravicius@gmail.com>
2018-10-20 19:55:38 +0100

    initial commit

---
+++ PdfiumBindingGenerator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using CppSharp;
+using CppSharp.AST;
+using CppSharp.Generators;
+
+namespace PdfiumBindingGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(args));
+            }
+
+            var includeDir = args[0];
+
+            if (!Directory.Exists(includeDir))
+            {
+                throw new DirectoryNotFoundException(includeDir);
+            }
+
+            ConsoleDriver.Run(new PdfLbrary(includeDir));
+
+            Console.ReadLine();
+        }
+    }
+
+    internal class PdfLbrary : ILibrary
+    {
+        private readonly string includePath;
+
+        public PdfLbrary(string includePath)
+        {
+            this.includePath = includePath;
+        }
+ 
+        public void Setup(Driver driver)
+        {
+            var options = driver.Options;
+            options.GeneratorKind = GeneratorKind.CSharp;
+
+            var parserOptions = driver.ParserOptions;
+            parserOptions.AddIncludeDirs(includePath);
+
+            var module = options.AddModule("pdfium");
+
+            foreach (var file in Directory.GetFiles(includePath, "*.h"))
+            {
+                module.Headers.Add(file);
+            }
+        }
+
+        public void SetupPasses(Driver driver) { }
+
+        public void Preprocess(Driver driver, ASTContext ctx) { }
+
+        public void Postprocess(Driver driver, ASTContext ctx) { }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# PDFium binding generator
+
+## Instructions
+
+1) Build the solution (No .NET Core support for now)
+
+2) Point `.exe` file to your include file directory.
